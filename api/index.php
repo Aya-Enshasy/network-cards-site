39591c463e08dc7132d472be094c9c0e63e8aca7
QusaiEnshasy <ayaenshasy2002@gmail.com>
2026-06-08 20:38:44 +0200

    Fix Vercel Laravel routing

--- api/index.php
+++ api/index.php
@@ -1,9 +1,23 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
 foreach (['/tmp/views', '/tmp/cache'] as $directory) {
     if (! is_dir($directory)) {
         mkdir($directory, 0777, true);
     }
 }
 
-require __DIR__.'/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
